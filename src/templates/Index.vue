2e1f683248b6d16c922fef8343ac0c4bddf62967
skiglock <demix@xaker.ru>
2021-07-14 17:39:14 +0300

    add mixins to optimize

--- src/templates/Index.vue
+++ src/templates/Index.vue
@@ -33,51 +33,10 @@ query ($id: ID!) {
 </page-query>
 
 <script>
-import SectionServices from '@/components/Section/SectionServices/SectionServices'
-import SectionPlans from '@/components/Section/SectionPlans/SectionPlans'
-import SectionPrice from '@/components/Section/SectionPrice/SectionPrice'
-import SectionOther from '@/components/Section/SectionOther'
-import SectionResults from '@/components/Section/SectionResults/SectionResults'
-import SectionConsultation from '@/components/Section/SectionConsultation'
-import SectionQuote from '@/components/Section/SectionQuote'
-import SectionOrder from '@/components/Section/SectionOrder'
-import SectionHead from '@/components/Section/SectionHead'
-import SectionHeadTwo from '@/components/Section/SectionHeadTwo'
-import SectionHeadThree from '@/components/Section/SectionHeadThree'
-import SectionMessage from '@/components/Section/SectionMessage'
-import SectionCards from '@/components/Section/SectionCards/SectionCards'
-import SectionContacts from '@/components/Section/SectionContacts'
-import SectionFaq from '@/components/Section/SectionFaq/SectionFaq'
-import SectionTarget from '@/components/Section/SectionTarget'
-import SectionTeam from '@/components/Section/SectionTeam'
-import SectionClients from '@/components/Section/SectionClients/SectionClients'
-import SectionDemonstration from '@/components/Section/SectionDemonstration'
-import SectionBlog from '@/components/Section/SectionBlog/SectionBlog'
+import sections from '../mixins/sections'
 
 export default {
-  components: {
-    SectionServices,
-    SectionHeadTwo,
-    SectionPlans,
-    SectionPrice,
-    SectionOther,
-    SectionResults,
-    SectionConsultation,
-    SectionQuote,
-    SectionOrder,
-    SectionHead,
-    SectionCards,
-    SectionContacts,
-    SectionFaq,
-    SectionHeadThree,
-    SectionMessage,
-    SectionTarget,
-    SectionTeam,
-    SectionClients,
-    SectionDemonstration,
-    SectionBlog
-  },
-  props: ['components'],
+  mixins: [sections],
   metaInfo() {
     return {
       title: this.$page.index.title
@@ -85,5 +44,3 @@ export default {
   }
 }
 </script>
-
-<style></style>
