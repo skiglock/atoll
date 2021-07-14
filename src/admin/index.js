a175db76537468ad1750bff91a7fa42d9c123864
skiglock <demix@xaker.ru>
2021-07-14 19:15:21 +0300

    test admin

--- src/admin/index.js
+++ src/admin/index.js
@@ -3,23 +3,23 @@ import CMS from 'netlify-cms'
 import { IdControl, IdPreview } from './widgets/uuid-widget'
 
 import { ru } from 'netlify-cms-locales'
-// import products from './preview/products/preview'
-// import blog from './preview/blog'
+import products from './preview/products/preview'
+import blog from './preview/blog'
 
-// import cases from './preview/case'
+import cases from './preview/case'
 
 CMS.registerLocale('ru', ru)
 
 CMS.registerWidget('id', IdControl, IdPreview)
 
-// CMS.registerPreviewStyle('/preview/style.css')
+CMS.registerPreviewStyle('/preview/style.css')
 
-// CMS.registerPreviewTemplate('products', products)
+CMS.registerPreviewTemplate('products', products)
 
 // CMS.registerPreviewTemplate('home', products)
 
 // CMS.registerPreviewTemplate('about', products)
 
-// CMS.registerPreviewTemplate('blog', blog)
+CMS.registerPreviewTemplate('blog', blog)
 
-// CMS.registerPreviewTemplate('cases', cases)
+CMS.registerPreviewTemplate('cases', cases)
