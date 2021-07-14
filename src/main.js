4f667c87a684ecbfa318d3dc814fb452e70f66e0
skiglock <demix@xaker.ru>
2021-07-14 17:51:14 +0300

    optimize fonts

--- src/main.js
+++ src/main.js
@@ -7,21 +7,43 @@ import Vuex from 'vuex'
 import store from '~/store'
 
 import InfiniteLoading from 'vue-infinite-loading'
+import ralewaylight from '@/assets/fonts/ralewaylight.woff2"'
+import ralewayregular from '@/assets/fonts/ralewayregular.woff2'
+import ralewaymedium from '@/assets/fonts/ralewaymedium.woff2'
+import ralewaysemibold from '@/assets/fonts/ralewaysemibold.woff2'
+import ralewaybold from '@/assets/fonts/ralewaybold.woff2'
 
 export default function (Vue, { head, appOptions }) {
   head.htmlAttrs = { lang: 'ru' }
   head.link.push({
-    rel: 'preconnect',
-    href: 'https://fonts.googleapis.com'
-  })
-  head.link.push({
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
+    rel: 'preload',
+    href: ralewaylight,
+    as: 'font',
     crossorigin: true
   })
   head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'
+    rel: 'preload',
+    href: ralewayregular,
+    as: 'font',
+    crossorigin: true
+  })
+  head.link.push({
+    rel: 'preload',
+    href: ralewaymedium,
+    as: 'font',
+    crossorigin: true
+  })
+  head.link.push({
+    rel: 'preload',
+    href: ralewaysemibold,
+    as: 'font',
+    crossorigin: true
+  })
+  head.link.push({
+    rel: 'preload',
+    href: ralewaybold,
+    as: 'font',
+    crossorigin: true
   })
   Vue.component('Layout', DefaultLayout)
   Vue.prototype.$getPath = getPath
